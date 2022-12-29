108bf9e785828719f06f9fd7f2812f791bbbcf14
Oleksandr <oleksandr.pavlyk03@gmail.com>
2022-12-29 22:09:03 +0200

    add lab files

---
+++ .hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/sasha/test2/hello.ko := ccache arm-linux-gnueabihf-ld -r -EL -z noexecstack -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/sasha/test2/hello.ko /home/sasha/test2/hello.o /home/sasha/test2/hello.mod.o ;  true
---
+++ .tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/sasha/test2/hello.ko
+/home/sasha/test2/hello.o
+
